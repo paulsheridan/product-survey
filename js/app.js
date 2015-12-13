206cd79c587df71f30bb7d96faaaee6562480884
Paul Sheridan <paul.sheridan@me.com>
2015-12-12 18:03:36 -0800

    It's posting a picture to the page, which is great. But the picture is really really big. Easy fix

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+var product1;
+var product2;
+var product3;
+var products = [];
+var idOne = document.getElementById("id-one");
+var idTwo = document.getElementById("id-two");
+var idThree = document.getElementById("id-three");
 var images = [
   "bag.jpg",
   "boots.jpg",
@@ -14,7 +21,6 @@ var images = [
   "usb.gif",
   "wine_glass.jpg"
 ];
-var products = [];
 
 function ProductBox (imageName){
   this.imageName = imageName;
@@ -23,16 +29,36 @@ function ProductBox (imageName){
   this.voteTotal = 0;
 }
 
-ProductBox.prototype.imagePath = function(name){
+ProductBox.prototype.imagePath = function (name){
   this.filePath = "img/" + name;
   this.productName = name.slice(0, -4);
 }
 
-function generateProducts (){
+function makeProducts (){
   for (var i = 0; i < images.length; i++){
     products[i] = new ProductBox (images[i]);
     products[i].imagePath(products[i].imageName);
-    console.log(products[i])
   }
 }
-generateProducts();
+makeProducts();
+
+function randNum (min, max){
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateChoices (){
+  product1 = products[randNum(0, products.length)];
+  product2 = products[randNum(0, products.length)];
+  product3 = products[randNum(0, products.length)];
+  while (product1 === product2 || product1 === product3){
+    product1 = products[randNum(0, products.length)];
+  }
+  while (product2 === product3){
+    product2 = products[randNum(0, products.length)];
+  }
+  imgOne = document.createElement("img");
+  imgOne.src = product1.filePath;
+  idOne.appendChild(imgOne);
+
+}
+generateChoices();
