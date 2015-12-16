d607af97252d4d4e0404098176727b76cc2e2b84
Paul Sheridan <paul.sheridan@me.com>
2015-12-15 20:26:17 -0800

    moved most functions into objects, got chart working

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
-var product1;
-var product2;
-var product3;
-var totalVotes = 0;
-var voteBox = document.getElementById("vote-box");
-var totalsButton = document.getElementById("see-totals");
-var totalEl = document.getElementById("totals");
 var allProducts = [];
 var images = [
   "bag.jpg",
@@ -22,137 +15,104 @@ var images = [
   "usb.gif",
   "wine_glass.jpg"
 ];
+var barData = {
+  labels : [],
+  datasets : [
+    {
+      fillColor : "#48A497",
+      strokeColor : "#48A4D1",
+      data : []
+    },
+    {
+      fillColor : "rgba(73,188,170,0.4)",
+      strokeColor : "rgba(72,174,209,0.4)",
+      data : []
+    }
+  ]
+}
 
-function Product (imageName){
-  this.imageName = imageName;
+function Product (name, path){
+  this.name = name;
+  this.path = path;
   this.voteTotal = 0;
   this.displayTotal = 0;
-  this.filePath;
-  this.productName;
 }
 
-Product.prototype.imagePath = function (name){
-  this.filePath = "img/" + name;
-  this.productName = name.slice(0, -4);
-}
-
-function popProducts (){
+(function makeProducts (){
   for (var i = 0; i < images.length; i++){
-    allProducts[i] = new Product (images[i]);
-    allProducts[i].imagePath(allProducts[i].imageName);
+    allProducts.push(new Product(images[i].slice(0, -4), "img/" + images[i]));
   }
-}
+})();
 
-function randNum (min, max){
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+var tracker = {
+  left: '',
+  right: '',
+  totalVotes: 0,
+  voteBox: document.getElementById("vote-box"),
+  totalsButton: document.getElementById("see-totals"),
+  totalEl: document.getElementById("totals"),
+  product1: document.getElementById("choice-one"),
+  product2: document.getElementById("choice-two"),
+  product3: document.getElementById("choice-three"),
 
-function generateNew (){
-  while (totalEl.firstChild) {
-    totalEl.removeChild(totalEl.firstChild);
-  }
+  randomNumber: function(min, max){
+    return Math.floor(Math.random() * (max - min)) + min;
+  },
 
-  var totVotes = document.getElementById("tot-votes");
-  var imgOne = document.getElementById("choice-one");
-  var imgTwo = document.getElementById("choice-two");
-  var imgThree = document.getElementById("choice-three");
+  generateChoices: function(){
+    do {
+      tracker.product1 = allProducts[tracker.randomNumber(0, allProducts.length)];
+      tracker.product2 = allProducts[tracker.randomNumber(0, allProducts.length)];
+      tracker.product3 = allProducts[tracker.randomNumber(0, allProducts.length)];
+    } while (tracker.product1 === tracker.product2 || tracker.product1 === tracker.product3 || tracker.product2 === tracker.product3);
 
-  totVotes.textContent = "You've voted " + totalVotes + " times.";
+    document.getElementById("choice-one").src = tracker.product1.path;
+    tracker.product1.displayTotal += 1;
+    document.getElementById("choice-two").src = tracker.product2.path;
+    tracker.product2.displayTotal += 1;
+    document.getElementById("choice-three").src = tracker.product3.path;
+    tracker.product3.displayTotal += 1;
 
-  product1 = allProducts[randNum(0, allProducts.length)];
-  product2 = allProducts[randNum(0, allProducts.length)];
-  product3 = allProducts[randNum(0, allProducts.length)];
-  while (product1 === product2 || product1 === product3){
-    product1 = allProducts[randNum(0, allProducts.length)];
-  }
-  while (product2 === product3){
-    product2 = allProducts[randNum(0, allProducts.length)];
-  }
-
-  imgOne.src = product1.filePath;
-  product1.displayTotal += 1;
-  imgTwo.src = product2.filePath;
-  product2.displayTotal += 1;
-  imgThree.src = product3.filePath;
-  product3.displayTotal += 1;
-
-  if (totalVotes > 1 && totalVotes % 15 === 0){
-    totalsButton.hidden = false;
-  } else {
-    totalsButton.hidden = true;
-  }
-}
-
-totalsButton.addEventListener("click", function(event){
-  allProducts.sort(compare);
-  var headArr = ["Product", "Votes", "Views", "Percentage", "% Per View"];
-  var tblEl = document.createElement("table");
-  for (var i = 0; i < headArr.length; i++){
-    var thEl = document.createElement("th");
-    thEl.textContent = headArr[i];
-    tblEl.appendChild(thEl);
-  }
-  for (var i = 0; i < allProducts.length; i++){
-    trEl = document.createElement("tr");
-    var valArr = [];
-    valArr.push(
-      allProducts[i].productName,
-      allProducts[i].voteTotal + " Votes",
-      allProducts[i].displayTotal + " Views",
-      Math.floor((allProducts[i].voteTotal / totalVotes) * 100) + "%",
-      Math.floor((allProducts[i].voteTotal / allProducts[i].displayTotal) * 100) + "%"
-    );
-    for (var j = 0; j < valArr.length; j++){
-      tdEl = document.createElement("td");
-      tdEl.textContent = valArr[j];
-      trEl.appendChild(tdEl);
-      tblEl.appendChild(trEl);
+    if (tracker.totalVotes > 1 && tracker.totalVotes % 15 === 0){
+      tracker.totalsButton.hidden = false;
+    } else {
+      tracker.totalsButton.hidden = true;
     }
-  }
-  totalEl.appendChild(tblEl);
-});
+  },
 
-voteBox.addEventListener("click", function(event){
-  if (event.target.id === "choice-one"){
-    product1.voteTotal += 1;
-    totalVotes += 1;
-  } else if (event.target.id === "choice-two"){
-    product2.voteTotal += 1;
-    totalVotes += 1;
-  } else if (event.target.id === "choice-three"){
-    product3.voteTotal += 1;
-    totalVotes += 1;
+  addTotalVote: function(productNumber){
+    productNumber.voteTotal += 1;
+    tracker.totalVotes += 1;
+    tracker.generateChoices();
   }
-  generateNew();
-});
+}
 
 function compare(a,b) {
   if (a.voteTotal > b.voteTotal)
-    return -1;
+  return -1;
   if (a.voteTotal < b.voteTotal)
-    return 1;
+  return 1;
   return 0;
 }
 
+tracker.totalsButton.addEventListener("click", function(event){
+  allProducts.sort(compare);
+  for (var i = 0; i < allProducts.length; i++){
+    barData.labels[i] = (allProducts[i].name);
+    barData.datasets[0].data[i] = (Math.floor((allProducts[i].voteTotal / tracker.totalVotes) * 100));
+    barData.datasets[1].data[i] = (Math.floor((allProducts[i].voteTotal / allProducts[i].displayTotal) * 100));
+  }
+  var chart = document.getElementById("canvas").getContext("2d");
+  new Chart(chart).Bar(barData);
+});
 
-var myChart = document.getElementById("myChart").getContext("2d");
-new Chart(myChart).Bar(barData);
-var barData = {
-	labels : ["January","February","March","April","May","June"],
-	datasets : [
-		{
-			fillColor : "#48A497",
-			strokeColor : "#48A4D1",
-			data : [456,479,324,569,702,600]
-		},
-		{
-			fillColor : "rgba(73,188,170,0.4)",
-			strokeColor : "rgba(72,174,209,0.4)",
-			data : [364,504,605,400,345,320]
-		}
-
-	]
-}
-
-popProducts();
-generateNew();
+tracker.voteBox.addEventListener("click", function(event){
+  if (event.target.id === "choice-one"){
+    tracker.addTotalVote(tracker.product1);
+  } else if (event.target.id === "choice-two"){
+    tracker.addTotalVote(tracker.product2);
+  } else if (event.target.id === "choice-three"){
+    tracker.addTotalVote(tracker.product3);
+  }
+});
+tracker.generateChoices();
